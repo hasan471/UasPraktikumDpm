4ad16ab2f8618df10f89bda4316ea777add0a2f3
mfitrahmaulana <mfitrahmaulana@student.uir.ac.id>
2024-01-18 17:56:13 +0700

    Tugas DPM

---
+++ src/pages/HomeScreen.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+
+const HomeScreen = ({ navigation }) => {
+  const [userData, setUserData] = useState('');
+
+  async function getData() {
+    try {
+      const token = await AsyncStorage.getItem('token');
+      console.log(token);
+      const response = await axios.post('http://localhost:5001/userdata', {
+        token: token,
+      });
+      console.log(response.data);
+      setUserData(response.data.data);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      getData();
+    });
+    return unsubscribe;
+  }, [navigation]);
+
+ 
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Home</Text>
+      <Text style={styles.text}>{userData && userData.nama}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+});
+
+export default HomeScreen;
